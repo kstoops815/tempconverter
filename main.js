20961bfaa0f94382b29c55eb39e0411b251e113e
Kelly Stoops <kstoops815@gmail.com>
2017-08-05 13:56:25 -0500

    completing the temp exercise

--- main.js
+++ main.js
@@ -1,32 +1,65 @@
-var inputTemp = document.getElementById("inputTemp").value;
+
 var outputTemp = 0;
+
+var button = document.getElementById("converter");
+
+var typeOfTemp;
+
+var convertedTypeOfTemp;
+
+
+button.addEventListener("click", determineConverter);
+
+
+
+function determineConverter () {
+	var inputTemp = document.getElementById("inputTemp").value;
+
+	if(document.getElementById("far").checked){
+			toCelsius(inputTemp);
+			typeOfTemp = document.getElementById("far").value;
+			convertedTypeOfTemp = document.getElementById("celsius").value;
+	} else if (document.getElementById("celsius").checked){
+			toFahrenheit(inputTemp);
+			typeOfTemp = document.getElementById("celsius").value;
+			convertedTypeOfTemp = document.getElementById("far").value;
+	}	else {
+			alert("You must select a temperature type.");
+	}
+
+	
+	colorAssignment(inputTemp, outputTemp);
+}
+
+
+
+
+	
+
 function toCelsius (inputTemp) {
-	outputTemp =Math.round(((inputTemp - 32) * 5)/9);
-	return outputTemp;
+	outputTemp =Math.round((inputTemp - 32) * (5/9));
 }
 
 function toFahrenheit (inputTemp) {
-	outputTemp = Math.round(((inputTemp * 9)/5)+32);
-	return outputTemp;
+	outputTemp = Math.round((inputTemp * (9/5)+32));
 }
 
-// Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
-
-// This function should determine which conversion should
-// happen based on which radio button is selected.
-function determineConverter (clickEvent) {
-	var tempType = ;
-	if(tempType===tempCel){
-		toFahrenheit(inputTemp);
-	} else if (tempType===tempFar){
-		toCelsius(inputTemp);
+function colorAssignment (inputTemp, outputTemp){
+	var color
+	if((outputTemp>90 && document.getElementById("celsius").checked) || (outputTemp>32 && document.getElementById("far").checked)){
+		color = "hot";
+	} else if ((outputTemp<32 && document.getElementById("celsius").checked) || (outputTemp<0 && document.getElementById("far").checked)){
+		color = "cold";
 	} else {
-	alert("You must select a temperature type!");
-	}
+		color = "comfy";
+		}
+	
+	document.getElementById("tempStatement").innerHTML = "The temperature of " + inputTemp + "&deg " + typeOfTemp + " is equal to <span class=" + color + "> " +outputTemp + "&deg " + convertedTypeOfTemp + "</span>!";
 }
 
-// Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter(inputTemp));
-
-//console.log("event", clickEvent);
+// if(outputTemp>90 && document.getElementById("far").checked || outputTemp>32 && document.getElementById("celsius").checked){
+// 		color = "hot";
+// 	} else if (outputTemp<32 && document.getElementById("far").checked || outputTemp<32 && document.getElementById("celsius").checked){
+// 		color = "cold";
+// 	} else if (outputTemp<90 && outputTemp>32 && document.getElementById("far").checked || outputTemp<32 && outputTemp>0 && document.getElementById("celsius").checked){
+// 		color = "comfy";
